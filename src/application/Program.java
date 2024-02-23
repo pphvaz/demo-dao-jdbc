e2f4b5d0136ef44cba4c044b044923ca81194013
Pedro Alves <pedrohvs.alves@gmail.com>
2024-02-23 17:02:05 +0000

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,5 +26,15 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== TESTE 3: seller FindAll ===");
+		
+		list = sellerDao.findAll();
+		
+		for (Seller obj:list) {
+			System.out.println(obj);
+		}
+		
+		
+		
 	}
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -94,8 +94,7 @@ public class Seller implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + ", department=" + department + "]";
+		return name + " (" + email + ") - " + department;
 	}
 	
 	
